cdc31bebf015cab7c7f730bfc537d5da621fd6e7
Rodion-24 <your-email@example.com>
2025-10-28 03:10:28 +0100

    add endpoints

--- FlightPlanApi/Program.cs
+++ FlightPlanApi/Program.cs
@@ -1,12 +1,17 @@
+using FlightPlanApi.Data;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var mongoConnectionString = builder.Configuration.GetConnectionString("MongoDb");
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IMongoClient>(sp => new MongoClient(mongoConnectionString));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
+builder.Services.AddScoped<IDatabaseAdapter, MongoDbDatabase>();
 
 var app = builder.Build();
 
